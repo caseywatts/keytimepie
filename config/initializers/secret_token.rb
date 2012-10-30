931db7ba57819ecdf2c03b48c171d3c07e7e4f3c
caseywatts <casey.s.watts@gmail.com>
2012-10-30 14:21:00 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Keytimepie::Application.config.secret_token = 'e92cfdf704a769daddc8f25a1f73d425927fcabddcf1463019013cd584b01bac9123ae2408c8a8620a1237e8712f093ef3c732974442122fdc49b83a96757c1f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Keytimepie::Application.config.session_store :cookie_store, key: '_keytimepie_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Keytimepie::Application.config.session_store :active_record_store
